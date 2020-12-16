ae955f8fd2d3b8e56de9ea41ecb74a742c809a1a
Tadeo Aguilar <tadeo.aguilar@finning.com>
2020-12-15 21:40:48 -0500

    uno

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/hello', function (req, res) {
+  res.json('Hello World!')
+})
+module.exports = router;
